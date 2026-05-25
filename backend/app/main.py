9df961d13c1d6dc907611f0d4f844f0993ba9ae9
Joseph-ZR <JosephRangel1324@gmail.com>
2026-05-24 22:14:38 -0700

    Setup backend structure and database connection

---
+++ backend/app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from .database import engine, Base
+from .models import User
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Backend running"}
